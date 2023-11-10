ac219310fa8e2890167ce2982c0d61084b5d9f26
Mateusz Bąk <61119842+matbak177@users.noreply.github.com>
2023-11-10 22:25:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,80 @@
-from flask import Flask, request, url_for
-from datetime import datetime
+from flask import Flask, request, render_template
+import pandas as pd
+import mysql.connector
+import pymysql
+import os
+
+class SendMail():
+    
+    def __init__(self,query):
+        self.query=query
+        
+    @staticmethod    
+    def MySQLConnect():
+        mydb = mysql.connector.connect(
+            user='mateusz'
+            ,password=''
+            ,host='localhost'
+            
+            ,database='python'
+        )
+    
+        return mydb
+    
+    def read_data(self):
+        df = pd.read_sql_query(self.query,self.MySQLConnect())
+        return df
+            
+df = SendMail('Select * from team').read_data()
+
+dict= {}
+for i,j in zip(df['email'], df['name']):
+    
+    dict[i] = j
+    
+    print(i, j)
+
+print(dict)
+
+options_names = {
+    "option1": "Opcja 1",
+    "option2": "Opcja 2",
+    "option3": "Opcja 3",
+    "option4": "Opcja 4",
+    "option5": "Opcja 5",
+    "option6": "Opcja 6",
+    "option7": "Opcja 7",
+}
 
 app = Flask(__name__) 
- 
+
 @app.route('/') 
 def index(): 
-    return '<h1>Hello World</h1>'
 
-@app.route('/cantor/<currency>/<amount>')
-def cantor(currency, amount):
-    message = f'<h1>You selected {currency} and amount {amount}</h1>'
-    return message
+    return '<h1>Druga strona: {}</h1>'.format('test')  
 
-@app.route('/about') 
-def about(): 
-    return '<h1>We are progremmers</h1>'
+@app.route('/proby/<string:email>')
+def proby(email):
     
+    return render_template("start.html", email=email)
+    # return render_template("zaczynamy.html", email=email)
+
+
+@app.route('/confirmation',methods = ['POST'])
+def confirmation():
+    
+    subordinate = request.form['subordinate']
+    employee = request.form.getlist('employee')
+    
+    # subordinate = options_names.get(request.form['subordinate'], "Unknown")
+    # employee_values = request.form.getlist('employee')
+    # employee_names = [options_names.get(value, "Unknown") for value in employee_values]
+        
+    body = f'You want to exchange {subordinate} for {", ".join(employee)}'
+    # body = f'You want to exchange {subordinate} for {", ".join(employee_names)}'
+
+    return body
+
+
 if __name__ == '__main__':
     app.run()
