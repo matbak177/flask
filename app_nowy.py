f7fdde83d23ddd6ea2eb879bba02186a03cea0f6
Mateusz Bąk <61119842+matbak177@users.noreply.github.com>
2023-11-23 17:10:16 +0100

    Update app_nowy.py

--- app_nowy.py
+++ app_nowy.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, url_for, redirect, render_template
 from datetime import datetime
 import pandas as pd
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    user='mateusz'
+    ,password='oliverzamek7'
+    ,host='localhost'
+    ,database='python'
+)
+
+cursor = mydb.cursor()
 
 ids = {
     "aaa": "mateusz.a.bak@gmail.com",
@@ -34,6 +44,7 @@ def page1(id):
     email = df[df['id'] == id]['email'].values[0] if id in df['id'].values else None
     return render_template('page1.html', id=id, email=email)
 
+
 @app.route('/page2', methods=['GET', 'POST'])
 def page2():
     if request.method == 'POST':
@@ -41,16 +52,37 @@ def page2():
         feedback_type = request.form['feedback_type']
         return render_template('page2.html',  email=email, feedback_type=feedback_type, subordinates=subordinates) # id=request.args.get('id'),
     return redirect(url_for('index'))
+    
 
-@app.route('/page3', methods=['GET', 'POST'])
+@app.route('/page3', methods=['POST'])
 def page3():
     if request.method == 'POST':
         subordinate = request.form['subordinate']
         employee = request.form.getlist('employee')
         email = request.form['email']
         feedback_type = request.form['feedback_type']
-        return render_template('page3.html', email=email, feedback_type=feedback_type, subordinate=subordinate, employee=employee) 
+
+        insert_query = '''
+        INSERT INTO feedback_data (timestamp, email, feedback_type, subordinate, employees)
+        VALUES (%s, %s, %s, %s, %s)
+        '''
+        cursor.execute(insert_query, (datetime.now(), email, feedback_type, subordinate, ', '.join(employee)))
+        mydb.commit()
+
+        return render_template('page3.html', email=email, feedback_type=feedback_type, subordinate=subordinate, employee=employee)
     return redirect(url_for('index'))
+
+@app.route('/history/<string:id>')
+def history(id):
+    email = df[df['id'] == id]['email'].values[0] if id in df['id'].values else None
+    
+    
+    query = "select * from python.feedback_data"
+    cursor.execute(query)
+    receiver_query = cursor.fetchall()
+    x = pd.read_sql_query(query,mydb)
+    
+    return render_template('history.html', id=id, email=email)
 
 if __name__ == '__main__': 
     app.run()
