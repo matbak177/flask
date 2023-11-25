ecc0b374b7f571514fc0a8d313da7daa15f46499
Mateusz Bąk <61119842+matbak177@users.noreply.github.com>
2023-11-25 16:59:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,79 +1,82 @@
-from flask import Flask, request, render_template
 import pandas as pd
-import mysql.connector
-import pymysql
-import os
+from flask import Flask, render_template, request, redirect, url_for, session
 
-class SendMail():
+app = Flask(__name__)
+
+# %% Logowanie
+users = [
+    ['mateusz.a.bak@gmail.com', 'mateuszbak', 'Mateusz Bak'],
+    ['kasia.h@gmail.com', 'kasiah', 'Katarzyna H'],
+    ["franek@gmail.com", 'franek', 'Franciszek Mostowiak']
+]
+
+user_df = pd.DataFrame(users, columns=['email', 'password', 'name'])
+
+# %% Pracownicy
+names = [
+       ['mateusz.a.bak@gmail.com','Marian X', 1],
+       ['mateusz.a.bak@gmail.com','Henryk P', 2],
+       ['mateusz.a.bak@gmail.com','Zbycho Z', 3],
+       ['mateusz.a.bak@gmail.com','Ilona B', 3],
+       ['kasia.h@gmail.com','Rafal P', 4],
+       ['kasia.h@gmail.com','Sandra Z',2],
+       ['kasia.h@gmail.com','Mariusz B',5],
+       ["franek@gmail.com", 'Stanislaw Z',6]
+       ]
+
+subordinates = pd.DataFrame(names, columns=['superior_email', 'subordinate', 'cl'])
+
+# %% Aplikacja
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # Sprawdź, czy istnieje taki użytkownik i czy hasło się zgadza
+    user = user_df[(user_df['email'] == email) & (user_df['password'] == password)]
+
+    if not user.empty:
+        # Ustaw sesję dla zalogowanego użytkownika
+        session['current_user'] = user['email'].values[0]
+        return redirect(url_for('home'))
+    else:
+        return "Hasło niepoprawne"
+
+@app.route('/home')
+def home():
+    # Sprawdź, czy użytkownik jest zalogowany
+    current_user = session.get('current_user')
+    if current_user:
+        user_info = user_df[user_df['email'] == current_user].iloc[0]
+        return render_template('home.html', user_info=user_info)
+    else:
+        return redirect(url_for('index'))  
     
-    def __init__(self,query):
-        self.query=query
-        
-    @staticmethod    
-    def MySQLConnect():
-        mydb = mysql.connector.connect(
-            user='mateusz'
-            ,password=''
-            ,host='localhost'
-            
-            ,database='python'
-        )
+@app.route('/ask_for_feedback')
+def ask_for_feedback():
+    # Sprawdź, czy użytkownik jest zalogowany
+    current_user = session.get('current_user')
+    if current_user:
+        # Pobierz listę podwładnych z bazy danych
+        # subordinates_list = subordinates[subordinates['superior_email'] == current_user]['subordinate'].tolist()
+        user_info = user_df[user_df['email'] == current_user].iloc[0]
+        return render_template('page1.html', user_info=user_info, subordinates=subordinates)
+    else:
+        return redirect(url_for('index'))  
+
+@app.route('/ask_for_feedback_2')
+def ask_for_feedback_2():
+    if request.method == 'POST':
+        return f'<h1>Tu będzie druga strona</h1>'
     
-        return mydb
-    
-    def read_data(self):
-        df = pd.read_sql_query(self.query,self.MySQLConnect())
-        return df
-            
-df = SendMail('Select * from team').read_data()
-
-dict= {}
-for i,j in zip(df['email'], df['name']):
-    
-    dict[i] = j
-    
-    print(i, j)
-
-print(dict)
-
-options_names = {
-    "option1": "Opcja 1",
-    "option2": "Opcja 2",
-    "option3": "Opcja 3",
-    "option4": "Opcja 4",
-    "option5": "Opcja 5",
-    "option6": "Opcja 6",
-    "option7": "Opcja 7",
-}
-
-app = Flask(__name__) 
-
-@app.route('/') 
-def index(): 
-
-    return '<h1>Druga strona: {}</h1>'.format('test')  
-
-@app.route('/proby/<string:email>')
-def proby(email):
-    
-    return render_template("start.html", email=email)
-    # return render_template("zaczynamy.html", email=email)
-
-
-@app.route('/confirmation',methods = ['POST'])
-def confirmation():
-    
-    subordinate = request.form['subordinate']
-    # subordinate = options_names.get(request.form['subordinate'], "Unknown")
-    
-    employee_values = request.form.getlist('employee')
-    employee_names = [options_names.get(value, "Unknown") for value in employee_values]
-        
-    # body = f'You want to exchange {subordinate} for {", ".join(employee)}'
-    body = f'You want to exchange {subordinate} for {", ".join(employee_names)}'
-
-    return body
-
+@app.route('/history') 
+def history(): 
+    return f'<h1>Tu bedzie historia</h1>'
 
 if __name__ == '__main__':
+    app.secret_key = 'tutaj_wpisz'
     app.run()
